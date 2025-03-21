628c2aaa6c93d9aff3180ad7f6acc54acd36c749
Eduardo Rodriguez <EduardoRodriguez@coherentsolutions.com>
2025-03-21 13:06:33 -0600

    Implemented circuit breaker and retry, as well as, GlobalExceptionHandler

---
+++ src/main/java/com/coherentsolutions/customer_service/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.coherentsolutions.customer_service.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ServiceUnavailableException.class)
+    public ResponseEntity<Object> handleServiceUnavailableException(ServiceUnavailableException ex, WebRequest request) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(String message, HttpStatus status) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+
+        return new ResponseEntity<>(body, status);
+    }
+}
--- src/main/java/com/coherentsolutions/customer_service/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/coherentsolutions/customer_service/service/impl/CustomerServiceImpl.java
@@ -2,11 +2,14 @@ package com.coherentsolutions.customer_service.service.impl;
 
 import com.coherentsolutions.customer_service.dto.CustomerDto;
 import com.coherentsolutions.customer_service.dto.RegisterCustomerDto;
+import com.coherentsolutions.customer_service.exceptionhandler.ServiceUnavailableException;
 import com.coherentsolutions.customer_service.mapper.CustomerMapper;
 import com.coherentsolutions.customer_service.model.Customer;
 import com.coherentsolutions.customer_service.repository.CustomerRepository;
 import com.coherentsolutions.customer_service.service.CustomerService;
 import com.coherentsolutions.customer_service.service.MembershipService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +26,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     private static final String CUSTOMERS_CACHE = "customersCache";
     private static final String DEFAULT_MEMBERSHIP_TYPE = "BASIC";
+    private static final String MEMBERSHIP_SERVICE = "membershipService";
     private static final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
     private final CustomerRepository customerRepository;
@@ -52,6 +56,8 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     @Transactional
+    @CircuitBreaker(name = MEMBERSHIP_SERVICE, fallbackMethod = "fallBackCreateMembership")
+    @Retry(name = MEMBERSHIP_SERVICE)
     public CustomerDto save(RegisterCustomerDto registerCustomerDto) {
         Customer customer = customerMapper.dtoToCustomer(registerCustomerDto);
         UUID membershipId = membershipService.createMembership(DEFAULT_MEMBERSHIP_TYPE);
@@ -76,5 +82,10 @@ public class CustomerServiceImpl implements CustomerService {
             return true;
         }
         return false;
+    }
+
+    public void fallBackCreateMembership(RegisterCustomerDto dto, Throwable t) {
+        log.error("Membership service is down.  Membership for Customer with email: {} not created.", dto.email());
+        throw new ServiceUnavailableException("Membership Service is currently unavailable. Please try again later.", t);
     }
 }
